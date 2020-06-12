5c3c0443a48d4c2a698d04c50163e69ad3389234
David Hernandez Azagury <davidhazagury@gmail.com>
2020-06-12 11:49:49 +0200

    Back End done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create, :delete]
   end
+
+  resources :doses, only: [:destroy]
 end
